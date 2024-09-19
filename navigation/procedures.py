7fa9062ba53ec3b48d95862348132a39031e7d87
PaulFilms <pablopila.spain@gmail.com>
2024-09-19 23:28:52 +0200

    Changes to be committed: 	modified:   navigation/home.py 	modified:   navigation/procedures.py 	new file:   resources/about.md 	modified:   test.ipynb

--- navigation/procedures.py
+++ navigation/procedures.py
@@ -4,7 +4,9 @@ from menus import *
 import os
 import pandas as pd
 
-from flexical.database import tables, enum_to_typedDict
+from flexical.database import tables, table_db #, enum_to_typedDict
+from flexical.report import REPORT
+from pycalibration.calibration import RESULTS
 
 ## SESSION STATES
 ## __________________________________________________________________________________________________
@@ -42,10 +44,24 @@ if PROCEDURE_ID:
     CURRENT_PROCEDURE = sql_row('PROCEDURES', 'Id', PROCEDURE_ID, st.session_state.PROCEDURES)[0]
     CURRENT_PROCEDURE = {field.name: CURRENT_PROCEDURE[field.name] for field in tables.PROCEDURES}
     
-    # st.write(SQL)
+    ## DB DATA
+    CURRENT_DB: dict = None
+    # st.write(CURRENT_TEMPLATE["DB"])
+    if isinstance(CURRENT_PROCEDURE[tables.PROCEDURES.DB.name], str):
+        # st.write('str')
+        try:
+            CURRENT_DB = json.loads(CURRENT_PROCEDURE[tables.PROCEDURES.DB.name])
+        except:
+            CURRENT_DB = dict()
+    elif isinstance(CURRENT_PROCEDURE[tables.PROCEDURES.DB.name], dict):
+        # st.write('dict')
+        CURRENT_DB = CURRENT_PROCEDURE[tables.PROCEDURES.DB.name]
+    else:
+        # st.write('nope')
+        CURRENT_DB = dict()
 
     st.text('')
-    tab_info, tab_testlist, tab_pydata = st.tabs([':material/info: INFO', ':material/lists: TEST', ':material/developer_mode: PYDATA'])
+    tab_info, tab_testlist, tab_scope, tab_pydata = st.tabs([':material/info: INFO', ':material/lists: TEST', ':material/arrow_range: SCOPE *CMC', ':material/developer_mode: PYDATA'])
 
 
     ## INFO
@@ -54,6 +70,45 @@ if PROCEDURE_ID:
     with tab_info:
         MD_EDITOR("PROCEDURES", PROCEDURE_ID, CURRENT_PROCEDURE[tables.PROCEDURES.INFO.name])
 
+    
+    ## TEST
+    ## __________________________________________________________________________________________________
+
+    with tab_testlist:
+
+        tx_title = st.text_input(label='DEFAULT TEST TITLE', value=CURRENT_PROCEDURE[tables.PROCEDURES.TITLE.name])
+        
+        result_types = [result.name for result in RESULTS.TYPES]
+        result_type = st.selectbox(
+            'TYPE OF RESULT', 
+            options=result_types, 
+            label_visibility='visible', 
+            index=result_types.index(CURRENT_DB[table_db.PROCEDURES.REPORT.name]['RESULT_TYPE'])
+            )
+        
+        abs_values = st.toggle('ABSOLUTE VALUES', value=CURRENT_DB[table_db.PROCEDURES.REPORT.name]['ABSOLUTE_VALUES'])
+
+        CURRENT_REPORT = CURRENT_DB[table_db.PROCEDURES.REPORT.name]
+
+        st.text("TEST REPORT")
+
+        with st.container(border=True):
+            TESTREPORT = REPORT.TESTREPORT.format(**CURRENT_REPORT['TESTREPORT'])
+            # TESTREPORT = REPORT.TESTREPORT.format(CURRENT_DB[table_db.PROCEDURES.REPORT.name][REPORT.TESTREPORT.])
+            
+            st.text_input(REPORT.TESTREPORT.fields.PARAMETERS.name, value=TESTREPORT.PARAMETERS)
+            st.text_input(REPORT.TESTREPORT.fields.MEASUREMENT.name, value=TESTREPORT.MEASUREMENT)
+            st.text_input(REPORT.TESTREPORT.fields.UNCERTAINTY.name, value=TESTREPORT.UNCERTAINTY)
+            st.text_input(REPORT.TESTREPORT.fields.LIMIT_OF_ERROR.name, value=TESTREPORT.LIMIT_OF_ERROR)
+            st.write(CURRENT_REPORT)
+        
+        st.text("MEASUREMENT UNITS")
+        with st.container(border=True):
+            for field in REPORT.MEASUREMENT_UNITS.fields:
+                st.text_input(field.name)
+
+        # st.line_chart()
+        # st.json(CURRENT_DB)
 
 
     ## PYDATA
---
+++ resources/about.md
@@ -0,0 +1,28 @@
+<!-- `English:` -->
+:blue-background[💊 ENGLISH:]
+
+Flexical is a solution for metroligia and calibration tasks. 
+
+The environment is made up of three modules: 
+
+    - Web Application
+    - Desktop Application
+    - Database
+  
+Flexical's advantage over the competition is...
+
+---
+
+<!-- `Spanish:`  -->
+:blue-background[💊 SPANISH:]
+
+Flexical es una solución para tareas de metroligia y calibración.
+
+El entorno se compone de tres modulos:
+
+    - Aplicación Web
+    - Aplicación de escritorio
+    - Base de datos
+
+La ventaja de Flexical frente a la competenecia es...
+
