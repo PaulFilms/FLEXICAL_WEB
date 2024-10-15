ba97058979462c74ba425c9d5c3a2862212f2ea5
PaulFilms <pablopila.spain@gmail.com>
2024-10-15 14:55:19 +0200

    Changes to be committed: 	modified:   navigation/calibrations.py 	modified:   navigation/settings.py 	new file:   resources/FLEXICAL.txt

--- navigation/calibrations.py
+++ navigation/calibrations.py
@@ -25,7 +25,55 @@ if 'CALIBRATIONS' not in st.session_state:
 # st.logo(os.path.join(path_resources, 'LOGO2.svg'))
 st.title('CALIBRATIONS PAGE')
 st.markdown('---')
-st.write('UNDER CONSTRUCTION')
+
+with st.expander(':material/publish: IMPORT CALIBRATIONS'):
+    st.file_uploader(
+        label='JSON FILE',
+        type=['json'],
+        accept_multiple_files=True
+    )
+
+# st.selectbox('')
+
+st.text('SEARCH CALIBRATION')
+with st.expander(':material/filter_list: FILTERS'):
+
+    st.text_input('CALIBRATION Id:')
+    st.text_input('DEVICE Id:')
+
+
+st.text('SEARCH CALIBRATION BY EVENT')
+EVENTS = ['ONSITE CAMPAIGN', 'REPAIR', 'EXT. CALIBRATION']
+cb_event = st.selectbox(
+    label='EVENT',
+    options=EVENTS,
+    index=None,
+    label_visibility='collapsed'
+)
+
+if cb_event == EVENTS[0]:
+    st.write(EVENTS[0])
+if cb_event == EVENTS[1]:
+    st.write(EVENTS[1])
+if cb_event == EVENTS[2]:
+    st.write(EVENTS[2])
+
+
+
+# with st.expander(':material/filter_list: FILTERS'):
+#     st.text_input('ONSITE CAMPAIGN:')
+#     st.text_input('SEND TO CALIBRATE:')
+
+
+
+## UNDER TEST
+## __________________________________________________________________________________________________
+
+
+st.markdown('---')
+
+
+st.write(':material/warning: UNDER CONSTRUCTION')
 t = '''
 In this page, depends of the user profile show diferent views:
 - Admin:
@@ -38,3 +86,4 @@ In this page, depends of the user profile show diferent views:
     - Asigned Projects
 '''
 st.caption(t)
+
--- navigation/settings.py
+++ navigation/settings.py
@@ -2,82 +2,126 @@ import os
 from menus import *
 import streamlit.components.v1 as components
 
-# st.logo(os.path.join(path_resources, 'LOGO2.svg'))
 
-if st.button(':material/database: GET DATABASE BACKUP', use_container_width=True):
-    pass
+## SESSION STATES
+## __________________________________________________________________________________________________
+
+if 'role' not in st.session_state:
+    st.session_state.role = None
+
+
+## PAGE
+## __________________________________________________________________________________________________
 
 st.title('SETTINGS PAGE')
 
-# Components Bootstrap
-st.subheader("Integrate Bootstrap")
-components.html(
-    """
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="https://cataas.com/cat?height=200" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">A Bootstrap Card</h5>
-    <p class="card-text">In Streamlit!</p>
-    <a href="https://cataas.com/" class="btn btn-primary">Go to cat API</a>
-  </div>
-</div>
-""",
-    height=400,
-)
+# st.logo(os.path.join(path_resources, 'LOGO2.svg'))
 
-# STYLE WITH CSS THROUGH MARKDOWN
-if st.checkbox("Make all buttons round", False):
-    st.markdown(
-        """
-    <style>
-    .stButton>button {
-        border-radius: 50%;
-    }
-    </style>
+with st.expander(':material/home_repair_service: GET DESKTOP APPLICATION'):
+    st.download_button(
+        ':material/download: DOWNLOAD FLEXICAL DESKTOP', 
+        data=os.path.join(path_resources, 'FLEXICAL.txt'),
+        file_name='FLEXICAL.txt',
+        use_container_width=True
+    )
+    st.file_uploader('UPLOAD FLEXICAL DESKTOP REQUEST')
+
+st.selectbox(':material/query_stats: KPIs', options=['Last 6 Months', 'Last 12 Months'], index=None)
+
+
+## UNDER TEST
+## __________________________________________________________________________________________________
+
+st.text('\n\n')
+st.title('UNDER TEST COMPONENTS')
+
+# if st.button(':material/database: GET DATABASE BACKUP', use_container_width=True):
+#     pass
+
+
+with st.expander('Under test components'):
+    # Components Bootstrap
+    st.subheader("Integrate Bootstrap")
+    components.html(
+    """
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="https://cataas.com/cat?height=200" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">A Bootstrap Card</h5>
+        <p class="card-text">In Streamlit!</p>
+        <a href="https://cataas.com/" class="btn btn-primary">Go to cat API</a>
+    </div>
+    </div>
     """,
-        unsafe_allow_html=True,
+        height=400,
     )
 
-# STYLE WITH JS THROUGH HTML IFRAME
-st.subheader("Style particular buttons")
+    # STYLE WITH CSS THROUGH MARKDOWN
+    if st.checkbox("Make all buttons round", False):
+        st.markdown(
+            """
+        <style>
+        .stButton>button {
+            border-radius: 50%;
+        }
+        </style>
+        """,
+            unsafe_allow_html=True,
+        )
 
-st.button("Hello Red")
-st.button("Hello Blue")
-st.button("Hello Green")
+    # STYLE WITH JS THROUGH HTML IFRAME
+    st.subheader("Style particular buttons")
 
-components.html(
-    """
-<script>
-const elements = window.parent.document.querySelectorAll('.stButton button')
-elements[0].style.backgroundColor = 'lightcoral'
-elements[1].style.backgroundColor = 'lightblue'
-elements[2].style.backgroundColor = 'lightgreen'
-</script>
-""",
-    height=0,
-    width=0,
-)
+    st.button("Hello Red")
+    st.button("Hello Blue")
+    st.button("Hello Green")
 
-if st.selectbox(
-    label='THEME',
-    options=['light', 'dark']
-    ):
-    pass
+    components.html(
+        """
+    <script>
+    const elements = window.parent.document.querySelectorAll('.stButton button')
+    elements[0].style.backgroundColor = 'lightcoral'
+    elements[1].style.backgroundColor = 'lightblue'
+    elements[2].style.backgroundColor = 'lightgreen'
+    </script>
+    """,
+        height=0,
+        width=0,
+    )
 
-components.html(
-'''
-<script>
-	import Widget from './Widget.svelte';
-</script>
+    if st.selectbox(
+        label='THEME',
+        options=['light', 'dark']
+        ):
+        pass
 
-<div>
-	<Widget />
-</div>
+    components.html(
+    '''
+    <script>
+        import Widget from './Widget.svelte';
+    </script>
 
-<div class="foo">
-	<button disabled>can't touch this</button>
-</div>
-''')
+    <div>
+        <Widget />
+    </div>
+
+    <div class="foo">
+        <button disabled>can't touch this</button>
+    </div>
+    ''')
+
+    # components.html(
+    # '''
+    # <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    # <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    # <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    # <ul class="nav nav-tabs">
+    #     <li role="presentation" class="active"><a href="#">Home</a></li>
+    #     <li role="presentation"><a href="#">Profile</a></li>
+    #     <li role="presentation"><a href="#">Messages</a></li>
+    # </ul>
+    # '''
+    # )
